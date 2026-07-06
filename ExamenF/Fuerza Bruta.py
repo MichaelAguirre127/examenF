b84943c97c49ccfae4d0912c3431547dba00539d
MichaelAguirre127 <michaelgabrielaguirreespino@gmail.com>
2026-07-05 19:25:15 -0500

    Guardar cambios

--- ExamenF/Fuerza Bruta.py
+++ ExamenF/Fuerza Bruta.py
@@ -41,86 +41,30 @@ def fuerza_bruta(monedas, i, j):
 
 
 def simulacion(monedas):
-
   turno = 1
-
-
-
   while monedas:
-
-
-
     if monedas[0] >= monedas[-1]:
-
       moneda = monedas.pop(0)
-
     else:
-
       moneda = monedas.pop()
-
-
-
     print(f"Turno del Jugador {turno}: tomó la moneda {moneda}")
-
-
-
     turno = 2 if turno == 1 else 1
-
-
-
-
-
 def main():
-
-
   print("    JUEGO DE MONEDAS")
-
   monedas = list(map(int, input("Ingrese las monedas: ").split()))
-
-
-
   resultado = fuerza_bruta(monedas, 0, len(monedas) - 1)
-
-
-
   print("\nSimulación del juego")
-
   simulacion(monedas.copy())
-
-
-
   jugador2 = sum(monedas) - resultado
-
-
-
   print("\nRESULTADO")
-
   print(f"Puntaje Jugador 1: {resultado}")
-
   print(f"Puntaje Jugador 2: {jugador2}")
-
-
-
   if resultado > jugador2:
-
     print("\n GANADOR: Jugador 1")
-
   elif jugador2 > resultado:
-
     print("\n  GANADOR: Jugador 2")
-
   else:
-
     print("\n  EMPATE")
-
-
-
   print("\nJuego terminado")
-
-
-
-
-
 if __name__ == "__main__":
-
   main()
